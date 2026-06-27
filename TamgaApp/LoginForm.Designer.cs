5eade5a09420a359ac5c4af42ef07bf7ac8e74a9
ugrylcn19 <ugrylcn19@hotmail.com>
2026-06-27 22:52:56 +0300

    v1.0.0.1

--- TamgaApp/LoginForm.Designer.cs
+++ TamgaApp/LoginForm.Designer.cs
@@ -73,7 +73,7 @@
             // 
             // btnGiris
             // 
-            this.btnGiris.BackColor = System.Drawing.Color.Crimson;
+            this.btnGiris.BackColor = System.Drawing.Color.Lime;
             this.btnGiris.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnGiris.Location = new System.Drawing.Point(437, 206);
             this.btnGiris.Name = "btnGiris";
